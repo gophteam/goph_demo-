f2f907a3e4f9ce79eb3057cb92c946e0a2cb5722
jerick <jerickgonito@gmail.com>
2020-08-08 12:29:49 +0800

    updates

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -7,11 +7,11 @@ pipeline {
 
     // set jenkins environment
     environment {
-        GOPATH = "${JENKINS_HOME}/workspace/${JOB_NAME}"
+        // GOPATH = "${JENKINS_HOME}/workspace/${JOB_NAME}"
         // GOPATH = "${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_ID}"
         // GOBIN = "${GOPATH}/bin"
-        CGO_ENABLED = 0
-        GO1136MODULE = "off"
+        // CGO_ENABLED = 0
+        // GO1136MODULE = "off"
     }
 
     stages {
